fb41be223e5d2cc84493db36aa993cc3e0fed53b
osakaishigaki <osakaishigaki@gmail.com>
2020-06-02 19:13:02 +0900

    tokei dekita

--- sample02/src/App.js
+++ sample02/src/App.js
@@ -3,32 +3,20 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  let items =[
-    {'name':'sstrobery','price':100},
-    {'name':'apple','price':150},
-    {'name':'banan','price':230},
-    
-  ]
-
   return(
-    <div className="App">
-      <table className="table table-striped">
-        <tbody>
-          {items.map((value)=>(
-            <tr>
-            <th scope="row">{value.name}</th>
-          <td>{value.price}yen</td>
-          </tr>
-          ))}
-
-        </tbody>
-
-      </table>
+    <div className="container text-center">
+      <Clock />
     </div>
   )
+}
 
-
-
+class Clock extends React.Component {
+  render() {
+    
+    this.now = new Date();
+    this.time = `${this.now.getHours()}:${this.now.getMinutes()}:${this.now.getSeconds()}`;
+  return <p>{this.time}</p>
+  }
 }
 
 export default App;
